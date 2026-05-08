f5963e701e07f843440c2b23e159de52a8f0ab5e
hengkaralaurenchia <hengkaralaurenchia@gmail.com>
2026-05-08 21:32:25 +0700

    add: github demo

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -5,7 +5,11 @@ import { Button } from "flowbite-react";
 
 export default function Component() {
   return (
-    <Navbar fluid rounded>
+    <Navbar 
+      fluid 
+      rounded 
+      className="fixed top-0 left-0 w-full z-50 bg-white/90 backdrop-blur-md shadow-sm transition-all duration-300"
+    >
       <NavbarBrand href="#">
         <img src="/projects/logo.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
         <span className="text-xs flex items-center gap-1 text-black">
@@ -13,19 +17,18 @@ export default function Component() {
           <HiCheckBadge className="text-blue-700" />
         </span>
       </NavbarBrand>
-        <div className="flex gap-3 text-xl justify-end">
-            <a href="https://github.com/hengkaralaurenchia" target="_blank">
-              <FaGithub className="cursor-pointer hover:scale-110 transition text-gray-900" />
-            </a>
-            <a href="https://www.instagram.com/laurnchia/" target="_blank">
-              <FaInstagram className="cursor-pointer hover:scale-110 transition text-pink-700" />
-            </a>
-            <a href="https://www.linkedin.com/in/hengkaralaurenchia" target="_blank">
-              <FaLinkedin className="cursor-pointer hover:scale-110 transition text-blue-800" />
-            </a>
-        </div>
-        {/* <p className="text-xs pb-3 text-[#779776] font-semibold"># Available to work</p> */}
-
+      
+      <div className="flex gap-3 text-xl justify-end">
+        <a href="https://github.com/hengkaralaurenchia" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="cursor-pointer hover:scale-110 transition text-gray-900" />
+        </a>
+        <a href="https://www.instagram.com/laurnchia/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="cursor-pointer hover:scale-110 transition text-pink-700" />
+        </a>
+        <a href="https://www.linkedin.com/in/hengkaralaurenchia" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="cursor-pointer hover:scale-110 transition text-blue-800" />
+        </a>
+      </div>
     </Navbar>
   );
 }
